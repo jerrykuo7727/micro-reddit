9633b8f2cd6d33061b9b030d70a220d5195bdd21
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-08-29 11:33:32 +0800

    Generate model Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829032540) do
+ActiveRecord::Schema.define(version: 20160829033254) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
