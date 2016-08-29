9d2a0e95b737d3a106aef1b4008e20f7f2695c1b
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-08-29 10:39:28 +0800

    Validate user.name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 6, maximum: 30 },
+                   format: { with: /[a-zA-Z\d-]+/ }
 end
