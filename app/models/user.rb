726e52df7fce69180eb84e14c83832f54bbbf830
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-08-29 10:48:42 +0800

    Update validation of user.email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
                    format: { with: /[a-zA-Z\d-]+/ },
                    uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :name, presence: true, length: { maximum: 255 },
-                   format: { with: VALID_EMAIL_REGEX }
-                   uniqueness: true
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
