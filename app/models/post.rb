f06fbd46846940c8a90b4dd4ee47117d1ddaf273
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-08-29 11:00:07 +0800

    Add validations of Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :text,  presence: true, length: { maximum: 10000 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 },
-                   format: { with: /[a-zA-Z\d-]+/ },
-                   uniqueness: true
+  validates :name,  presence: true, length: { maximum: 50 },
+                    format: { with: /[a-zA-Z\d-]+/ },
+                    uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
