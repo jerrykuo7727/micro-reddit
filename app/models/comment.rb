efe4df1370f74c562fd5d5c4840450ed7c3d168d
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-08-29 11:42:45 +0800

    Associate comments with users and posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   validates :text, presence: true, length: { maximum: 1000 }
+  belongs_to :user
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :text,  presence: true, length: { maximum: 10000 }
   belongs_to :user
+  has_many :comments
 end
